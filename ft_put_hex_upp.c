e8cdfc758e0d90c2e51ce686c5cbd3cc4da6e321
jcherrerosl <jcherrerosl@gmail.com>
2024-09-30 16:18:07 +0200

    arreglada len hex

--- ft_put_hex_upp.c
+++ ft_put_hex_upp.c
@@ -3,51 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_put_hex_upp.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juanherr <juanherr@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: juanherr <juanherr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 23:14:16 by juanherr          #+#    #+#             */
-/*   Updated: 2024/09/29 23:14:18 by juanherr         ###   ########.fr       */
+/*   Updated: 2024/09/30 16:15:24 by juanherr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_hexlen(unsigned int n)
-{
-	int	i;
-
-	i = 0;
-	while (n >= 16)
-	{
-		n = n / 16;
-		i++;
-	}
-	i++;
-	return (i);
-}
-
 int	ft_put_hex_upp(size_t n)
 {
-	int	len;
+	int		len;
+	char	*hex_digits;
 
-	len = ft_hexlen(n);
+	len = 0;
+	hex_digits = "0123456789ABCDEF";
 	if (n >= 16)
-	{
-		ft_put_hex_upp(n / 16);
-		ft_put_hex_upp(n % 16);
-	}
-	else
-	{
-		if (n < 10)
-		{
-			n = n + '0';
-			write (1, &n, 1);
-		}
-		else if (n >= 10 && n < 16)
-		{
-			n = n - 10 + 'A';
-			write (1, &n, 1);
-		}
-	}	
+		len += ft_put_hex_upp(n / 16);
+	len += ft_putchar(hex_digits[n % 16]);
 	return (len);
 }
